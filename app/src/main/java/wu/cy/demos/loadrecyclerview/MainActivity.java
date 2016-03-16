f5794061fa8e3c703ff794576d5e040ba1dca8fa
wuchengyun <yunnywu@gmail.com>
2016-03-16 22:31:18 +0800

    first commit

---
+++ app/src/main/java/wu/cy/demos/loadrecyclerview/MainActivity.java
@@ -0,0 +1,196 @@
+package wu.cy.demos.loadrecyclerview;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.drawable.Drawable;
+import android.os.Handler;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Observable;
+
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView mRecyclerView;
+
+    SwipeRefreshLayout mSwipeLayout;
+
+    Context mContext;
+
+    Handler mHandler = new Handler();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mContext = MainActivity.this;
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        mSwipeLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_layout);
+
+        mSwipeLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+
+                mHandler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mSwipeLayout.setRefreshing(false);
+                    }
+                }, 3000);
+
+            }
+        });
+
+
+
+
+        LinearLayoutManager lm = new LinearLayoutManager(mContext);
+        lm.setOrientation(LinearLayoutManager.VERTICAL);
+        mRecyclerView.setLayoutManager(lm);
+
+
+        mRecyclerView.setAdapter(new MyAdapter());
+
+        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+            }
+        });
+
+
+        getAppInfo(true,1);
+
+
+    }
+
+
+    class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+            TextView text;
+            ImageView icon;
+
+            public ViewHolder(View itemView) {
+                super(itemView);
+                icon = (ImageView) itemView.findViewById(R.id.iv);
+                text = (TextView) itemView.findViewById(R.id.item_text);
+            }
+        }
+
+
+        @Override
+        public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(MainActivity.this).inflate(R.layout.layout_item, parent,false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(MyAdapter.ViewHolder holder, int position) {
+
+            holder.text.setText(mInfos.get(position).name);
+            holder.icon.setImageDrawable(mInfos.get(position).icon);
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return mInfos.size();
+        }
+    }
+
+
+    class AppInfo {
+
+        public AppInfo(CharSequence name, Drawable icon) {
+            this.name = name;
+            this.icon = icon;
+        }
+
+        CharSequence name;
+        Drawable icon;
+    }
+
+
+    private boolean isRefresh;
+    private List<AppInfo> mInfos = new ArrayList<>();
+
+    private void getAppInfo(boolean refresh, int page) {
+        if (refresh) {
+            mInfos.clear();
+        }
+
+        rx.Observable observable = rx.Observable.create(new rx.Observable.OnSubscribe< List<AppInfo>>() {
+            @Override
+            public void call(Subscriber<? super  List<AppInfo>> subscriber) {
+                PackageManager pm = getPackageManager();
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_LAUNCHER);
+                List<ResolveInfo> infos = pm.queryIntentActivities(intent, 0);
+                List<AppInfo> datas = new ArrayList<AppInfo>(infos.size());
+                for(ResolveInfo info : infos){
+                    AppInfo app = new AppInfo(info.loadLabel(pm), info.loadIcon(pm));
+                    datas.add(app);
+
+                }
+                subscriber.onNext(datas);
+                subscriber.onCompleted();
+            }
+        });
+        observable.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<List<AppInfo>>() {
+                    @Override
+                    public void onCompleted() {
+//                        mSwipeLayout.setRefreshing(false);
+//                        mRecyclerView.getAdapter().notifyDataSetChanged();
+                        Toast.makeText(MainActivity.this, "onCompleted", Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Toast.makeText(MainActivity.this, "onError", Toast.LENGTH_LONG).show();
+                        e.printStackTrace();
+                    }
+
+                    @Override
+                    public void onNext( List<AppInfo> infos) {
+                        mInfos.addAll(infos);
+
+                    }
+
+
+                });
+
+
+    }
+
+
+}
